de4ba814b9c70b6a7fe5ebad5cf1e27e7e09fa2f
wangfei3 <wangfei3@xiaomi.com>
2016-12-27 09:49:28 +0800

    修改广告

---
+++ src/main/java/com/fly/web/controller/HitbController.java
@@ -0,0 +1,70 @@
+package com.fly.web.controller;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fly.HttpUtils;
+import com.fly.Response;
+
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+@Controller
+@RequestMapping("/hitb")
+public class HitbController {
+	@Autowired
+	private ShardedJedisPool shardedJedisPool;
+	@ResponseBody
+	@RequestMapping("/urltran.do")
+	public String urltran(String url) throws Throwable{
+		System.out.println("urltran url:"+url);
+		String s="";
+		CloseableHttpClient httpClient = HttpUtils.getHttpClient(5000);
+		for(int i=0;i<1;i++){
+		Response r = null;
+		CloseableHttpResponse response = null;
+		try {
+			ShardedJedis jedis = shardedJedisPool.getResource();
+			String cookie=jedis.get("mycookie");
+			System.out.println("urltran cookie:"+cookie);
+			jedis.close();
+			String turl="http://pub.alimama.com/urltrans/urltrans.json?siteid=3406996&adzoneid=22076161&promotionURL="+java.net.URLEncoder.encode(url,"utf-8")+"&t=1480063247548&pvid=&_tb_token_=DJWghTHg67q&_input_charset=utf-8";
+			System.out.println("urltran turl:"+turl);
+			HttpGet httpPost = new HttpGet(turl);
+			try {
+				httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+				httpPost.addHeader("Cookie",
+						cookie);
+				response = httpClient.execute(httpPost);
+				r = new Response();
+				r.setStatusCode(response.getStatusLine().getStatusCode());
+				if (response.getStatusLine().getStatusCode() == 200) {
+					r.setResponseString(EntityUtils.toString(response.getEntity()));
+					s = r.getResponseString();
+					return s;
+				}else{
+					throw new Throwable("111");
+				}
+				
+			} catch (Exception e) {
+				throw e;
+			} finally {
+				if (response != null) {
+					response.close();
+				}
+			}
+		} finally {
+			if (httpClient != null) {
+				httpClient.close();
+			}
+		}
+		}
+		return "";
+	}
+}
